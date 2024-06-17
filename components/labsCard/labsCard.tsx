cc91291bf5860b2b5b0a3e2369107baf6fec1162
tylerwest7 <tyler@tylerwest.co>
2024-06-17 19:34:44 -0400

    fixes apostrophes

--- components/labsCard/labsCard.tsx
+++ components/labsCard/labsCard.tsx
@@ -5,13 +5,13 @@ const LabsCard: React.FC = ({}) => {
     <div className="labs-card grid pt-56 pb-5">
       <h1 className="text-5xl font-medium text-center">Labs</h1>
       <h1 className="text-2xl text-center pt-5 w-6/12 m-auto">
-        Discover Tyler's explorative 3D works that push the boundaries of
+        Discover Tyler&apos;s explorative 3D works that push the boundaries of
         design, materials, lighting, composition, and simulations. Dive into a
         portfolio that showcases intricate designs, innovative material
         applications, stunning lighting techniques, masterful compositions, and
         dynamic simulations. Click here to explore the full collection and
-        experience the artistry and technical expertise that define Tyler's 3D
-        creations.
+        experience the artistry and technical expertise that define Tyler&apos;s
+        3D creations.
       </h1>
     </div>
   );
